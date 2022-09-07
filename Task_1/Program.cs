78e8c7edbb5be00a92fe8f62822869cef834cbdf
Lazarev Konstantin <klik@example.com>
2022-09-07 11:25:54 +0300

    Больше 0

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Программа запрашивает натуральное число M, 
+// пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.WriteLine("Введите количесво чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[M];
+int j = 0;
+
+for (int i = 0; i < M; i++)
+{
+   array[i] = Convert.ToDouble(Console.ReadLine());
+   if(array[i] > 0)
+   {
+        j++;
+   }
+}
+var str = string.Join(", ", array);
+Console.WriteLine($"[{str}]  ->  {j}");
