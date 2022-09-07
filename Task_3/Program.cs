5abb5de6b4c38541b1cb94dd25989c61ce298b61
Lazarev Konstantin <klik@example.com>
2022-09-07 11:28:32 +0300

    Сумма произведений

---
+++ Task_3/Program.cs
@@ -0,0 +1,25 @@
+﻿//  Найдите сумму произведений пар чисел в одномерном 
+//  целочисленном массиве. Парой считаем первый и 
+//  последний элемент, второй и предпоследний и т.д. 
+//  Если кол-во элементов нечетное, то элемент 
+//  посередине не учитывать.
+
+int N = 5;
+int[] array = new int[N];
+int pr;
+int sum = 0;
+
+Random rnd = new Random();
+
+for (int i = 0; i < N; i++)
+{
+    array[i] = rnd.Next(1, 10);
+}
+for (int i = 0; i < N / 2; i++)
+{
+    pr = array[i] * array[N - (i + 1)];
+    sum += pr;
+}
+
+var str = string.Join(", ", array);
+Console.WriteLine($"{str}  ->  {sum}");
